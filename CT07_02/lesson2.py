689b6c189f95fd1acf70ce38eb41f0cbd8d6510c
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-01-19 09:03:04 +0000

    Jan 19, 2025, 9:03 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -67,5 +67,5 @@ else:
 # Task 5
 
 toppings = input("What toppings would you like on your pizza? ")
-while True:
+while toppings:
     toppings = input("What other toppings would you like on your pizza? ")
