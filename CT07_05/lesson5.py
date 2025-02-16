0ae593a422b6468cf6aac3b5588a6c76444c01e8
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-02-16 09:01:24 +0000

    Feb 16, 2025, 9:01 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -68,8 +68,8 @@ print(name)
 pokemons = [
     "Pikachu", "Charizard", "Bulbasaur", "Squirtle", "Jigglypuff",
     "Meowth", "Psyduck", "Eevee", "Snorlax", "Mewtwo",
-    "Lapras", "Gengar", "Dragonite", "Machamp", "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
-    "Electabuzz"
+    "Lapras", "Gengar", "Dragonite", "Machamp", "Arcanine", 
+    "Alakazam", "Gyarados", "Vaporeon", "Scyther", "Electabuzz"
 ]
 
 powers = [
