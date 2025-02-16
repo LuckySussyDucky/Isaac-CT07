6b907ba3ea8abba85a69504811cacd8d04407a96
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-02-16 08:50:23 +0000

    Feb 16, 2025, 8:50 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -46,4 +46,4 @@ namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
 heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 
 maximum = max(heightlist)
-heightlist.index(maximum)
+counterheightlist.index(maximum)
