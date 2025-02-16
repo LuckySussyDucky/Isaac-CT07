7727e62df68af52c18e8b3841c2ac6744b42811d
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-02-16 08:16:08 +0000

    Feb 16, 2025, 8:16 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -10,5 +10,7 @@ while counter != 0:
     num = random.randint(1, 1000)
     if num in numbers:
         numbers.append(num)
-    counter = counter - 1
+    else:
+        numbers.append(num)
+        counter = counter - 1
 print(numbers)
