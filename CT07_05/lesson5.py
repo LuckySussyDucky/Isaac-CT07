2b0d7a0eb0741c384073bd0129f78e7ec77f990f
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-02-16 08:43:31 +0000

    Feb 16, 2025, 8:43 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -44,3 +44,4 @@ namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
             "Sophia", "Lucas", "Mia", "Aiden"
             ]
 heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+
