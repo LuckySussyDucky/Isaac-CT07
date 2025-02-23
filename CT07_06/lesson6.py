c42201cc3731e557b19f384d355da1dd12e7bde3
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-02-23 09:24:50 +0000

    23.2.2025

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,2 +1,52 @@
 print("Hello from lesson 6")
 
+# Task 1
+
+contacts = []
+contact1 = ["John", 98453126, "john@gmail.com"]
+contact2 = ["Adam", 93029102, "adam@gmail.com"]
+contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+contacts.append(contact1)
+contacts.append(contact2)
+contacts.append(contact3)
+print(contacts)
+
+for x in contacts:
+    print(x)
+    for y in x:
+        print(y)
+
+
+# Task 2a
+
+students = [
+    ["Olivia", "F"], ["Noah", "M"], ["Emma", "F"],
+    ["Liam", "M"], ["Ava", "F"], ["Ethan", "M"],
+    ["Sophia", "F"], ["Lucas", "M"], ["Mia", "F"],
+    ["Aiden", "M"], ["Isabella", "F"], ["Jackson", "M"],
+    ["Amelia", "F"], ["Logan", "M"], ["Lily", "F"]
+]
+boys = [
+]
+girls = [
+]
+counter1 = 0
+counter2 = 0
+for student in students:
+    name, gender = student
+    print(name, ", ", gender)
+
+
+# Task 2b
+
+    if gender == "F":
+        girls.append(name)
+        counter1 = counter1 + 1
+    elif gender == "M":
+        boys.append(name)
+        counter2 = counter2 + 1
+print(counter1)
+print(counter2)
+
+
+
--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,2 +1,45 @@
 print("Hello from lesson 7")
 
+# Task 1
+
+fruits1 = ["Apple", "Banana", "Cherry"]
+fruits2 = ["Durian", "Elderberry", "Figs"]
+fruits = [
+]
+fruits = fruits1 + fruits2
+print(fruits)
+
+
+# Task 2
+
+price1 = [3.20, 2.65, 1.75]
+price2 = [6.15, 5.45, 4.20]
+price = [
+]
+price = price1 + price2
+print(sorted(price))
+
+
+# Task 3
+
+fruits = ["Apple", "Banana", "Cherry", "Durian", "Elderberry", "Figs"]
+fruit1 = [
+]
+fruit2 = [
+]
+index = 3
+print(fruits[0:index])
+print(fruits[index:6])
+
+
+# Task 4
+
+lenH = len(fruits) // 2
+print(fruits[:lenH])
+print(fruits[lenH:])
+
+
+# Task 5
+
+fruit1 = ["Apple", "Banana", "Cherry", "Durian"]
+fruit2 = ["Cherry", "Durian", "Elderberry", "Figs"] 
