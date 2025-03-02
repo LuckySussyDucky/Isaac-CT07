79718bf5ad87c1a429ec3b6a7cea6e120996e77d
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-02 09:13:36 +0000

    Mar 2, 2025, 9:13 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -126,6 +126,9 @@ student2 = ["jared", "80584155", "Nerd Club"
 student3 = ["zhi wei", "97426101", "Goodminton"
 ]
 students = student1 + student2 + student3
+size = 3
+for i in range(0,len(students), size):
+    nestedList.append(students[i:i + size])
 for stuff in nestedList:
     for detail in stuff:
         print("Name: ", detail)
