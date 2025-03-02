96c229b900d21b3064bc1a62f401e72ac3fb0ff2
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-02 09:05:57 +0000

    Mar 2, 2025, 9:05 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -126,6 +126,7 @@ student2 = ["jared", "80584155", "Nerd Club"
 student3 = ["zhi wei", "97426101", "Goodminton"
 ]
 students = student1 + student2 + student3
+print(students)
 for stuff in students:
     print("Name: ", stuff)
     # for stuff in range: 
