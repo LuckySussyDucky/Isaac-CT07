8b8ed7369e05d1effe77ee557673b913b76f45f3
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-09 08:39:33 +0000

    Mar 9, 2025, 8:39 AM

--- CT07_MidSem/Practical.py
+++ CT07_MidSem/Practical.py
@@ -22,10 +22,10 @@ counter = 1
 while FoodToEat != "end":
     FoodToEat = input("What would you like to eat?")
     Order.append(FoodToEat)
+print("You have the following order:")
 Order.pop(len(Order-1))
 for i in range(len(Order)):
     print(counter, ".", Order[i])
 
 
 
-print("You have the following order:")
