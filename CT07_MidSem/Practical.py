3fb6c7123907d5c234906df685067655f148fd03
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-09 08:19:23 +0000

    Mar 9, 2025, 8:19 AM

--- CT07_MidSem/Practical.py
+++ CT07_MidSem/Practical.py
@@ -16,4 +16,4 @@ elif HeroHealth > 0:
     print("After fighting a monster, his Health is now: ", HeroHealth)
     MonstersFought = MonstersFought + 1
 else:
-    print("The Hero fought ", MonstersFought, )
+    print("The Hero fought ", MonstersFought, "battles, and died")
