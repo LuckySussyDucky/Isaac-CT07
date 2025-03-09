33acdc9831f21dfd51f0930f8b241c5374278941
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-09 08:23:29 +0000

    Mar 9, 2025, 8:23 AM

--- CT07_MidSem/Practical.py
+++ CT07_MidSem/Practical.py
@@ -5,17 +5,10 @@ MonstersFought = 0
 HeroHealth = 100
 print("Hero starts on his adventure with Health:", HeroHealth)
 
-while HeroHealth :
-    if HeroHealth != 0:
-        healthLost = random.randint(1, 15)
-        HeroHealth = HeroHealth - healthLost
-        print("After fighting a monster, his Health is now:", HeroHealth)
-        MonstersFought = MonstersFought + 1
-    elif HeroHealth > 0:
-        healthLost = random.randint(1, 15)
-        HeroHealth = HeroHealth - healthLost
-        print("After fighting a monster, his Health is now:", HeroHealth)
-        MonstersFought = MonstersFought + 1
-    else:
-        break
-print("The Hero fought ", MonstersFought, "battles, and died.")
+while HeroHealth > 0:
+    healthLost = random.randint(1, 15)
+    HeroHealth = HeroHealth - healthLost
+    print("After fighting a monster, his Health is now:", HeroHealth)
+    MonstersFought = MonstersFought + 1
+else:
+    print("The Hero fought ", MonstersFought, "battles, and died.")
