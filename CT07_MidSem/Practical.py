ab357585c76ad704b4d44ebf92995e5a529f9875
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-09 09:06:55 +0000

    Mar 9, 2025, 9:06 AM

--- CT07_MidSem/Practical.py
+++ CT07_MidSem/Practical.py
@@ -46,7 +46,7 @@ hiddenpwd = "computhink"
 
 # Password validation
 while not correct:
-	userpwd = input("What is the password? ")
+	    userpwd = input("What is the password? ")
 
 if userpwd == hiddenpwd:
 	correct = True
