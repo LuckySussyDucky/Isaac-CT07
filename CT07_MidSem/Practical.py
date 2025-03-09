cb20979387eae9bc8a290d531e4212e134a55059
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-09 08:18:18 +0000

    Mar 9, 2025, 8:18 AM

--- CT07_MidSem/Practical.py
+++ CT07_MidSem/Practical.py
@@ -10,3 +10,8 @@ if HeroHealth != 0:
     HeroHealth = HeroHealth - healthLost
     print("After fighting a monster, his Health is now: ", HeroHealth)
     MonstersFought = MonstersFought + 1
+elif HeroHealth > 0:
+    healthLost = random.randint(1, 15)
+    HeroHealth = HeroHealth - healthLost
+    print("After fighting a monster, his Health is now: ", HeroHealth)
+    MonstersFought = MonstersFought + 1
