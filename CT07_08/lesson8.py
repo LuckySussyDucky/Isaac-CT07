ec97ee17457478e6b1a544db6033e72a91c475a8
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-03-23 08:28:49 +0000

    Mar 23, 2025, 8:28 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -170,11 +170,10 @@ while True:
 # Task 8
 
 
-while True:
-    phrase = input("Provide a word: ")
-    if phrase != "end":
-        word = phrase.split(" ")
-        reversedWords = [
+phrase = input("Provide a word: ")
+if phrase != "end":
+    word = phrase.split(" ")
+    reversedWords = [
 ]
     for letters in word:
         reversedWord = letters[::-1]
