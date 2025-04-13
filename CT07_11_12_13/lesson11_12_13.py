9979ed5acfa25231b67b85010083820b101f9524
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-13 08:36:25 +0000

    Apr 13, 2025, 8:36 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -4,17 +4,17 @@ print("Hello from lesson 11_12_13")
 
 # Recap 1
 
-def diceGuess(guess):
-    randomNum = random.randint(1,6)
-    return guess == randomNum
+# def diceGuess(guess):
+#     randomNum = random.randint(1,6)
+#     return guess == randomNum
 
-num = 1
-for i in range(6):
-    if diceGuess(num):
-        num = num + 1
-        print("Correct!")
-    else:
-        print("Wrong!")
+# num = 1
+# for i in range(6):
+#     if diceGuess(num):
+#         num = num + 1
+#         print("Correct!")
+#     else:
+#         print("Wrong!")
     
 
     
