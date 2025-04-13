09f8a299a1806bc6a0bb5fadab8cca24d67beca4
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-13 09:20:38 +0000

    Apr 13, 2025, 9:20 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -34,7 +34,7 @@ def printBoard():
     for row in board:
         for cell in row:
             if cell != " ":
-                print(cell, end = " ")
+                print(str(cell), end = " ")
             else:
                 print("", cellNumber, "", end = " ")
             if cellNumber % 3 != 0:
