9eb8038900f1a46fda8020c986b042a3ba2643cb
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-13 09:14:58 +0000

    Apr 13, 2025, 9:14 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -40,7 +40,7 @@ def printBoard():
             if cellNumber % 3 != 0:
                 print(" | ", end = " ")
             cellNumber = cellNumber + 1
-            
+        
 
 
                 
