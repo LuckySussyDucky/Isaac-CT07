8d53bfaeee7456a040cff06c63dfd0a633790452
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-13 08:31:54 +0000

    Apr 13, 2025, 8:31 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -5,11 +5,11 @@ print("Hello from lesson 11_12_13")
 # Recap 1
 
 def diceGuess(guess):
-    num = random.randint(0,7)
+    randomNum = random.randint(0,7)
     return guess == num
 
 for i in range(6):
-    diceGuess()
+    diceGuess(num)
 
     
 # Task 1
