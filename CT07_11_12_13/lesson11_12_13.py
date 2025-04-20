82429f0c07e82c2c214d7cbbe496fdf24c7beeda
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-20 08:29:50 +0000

    Apr 20, 2025, 8:29 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -49,7 +49,7 @@ def printBoard(board):
 
 def playerMove():
     moveInput = input("Enter your move: (1 - 9): ")
-    if moveInput.isdigit() and :
+    if moveInput.isdigit() and moveInput < 10:
         move = int(moveInput) - 1
         row = move // 3
         column = move % 3
