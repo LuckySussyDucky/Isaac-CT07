5155bd3f18819ad3bf1a1d1373fdba3601845d1a
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-27 08:43:52 +0000

    Apr 27, 2025, 8:43 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -9,6 +9,7 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong Game")
+
 while running:
     for event in pygame.event.game():
         if event.type == pygame.QUIT:
