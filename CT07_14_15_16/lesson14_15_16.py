0b228161b598f48acaab4d98f9cbe40494c77c86
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-27 08:29:27 +0000

    Apr 27, 2025, 8:29 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -7,5 +7,5 @@ pygame.init()
 
 screen_width = 800
 screen_height = 600
-screen = pygame.display.set_mode(screen_width, screen_height)
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong Game")
