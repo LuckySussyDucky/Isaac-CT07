c92caac412ca6f4bee44c766a2356b2258bc81d9
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-27 08:36:41 +0000

    Apr 27, 2025, 8:36 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -4,10 +4,11 @@ print("Hello from lesson 14_15_16")
 # Task 1
 
 running = True
-pygame.init()
+while running:
 
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Pong Game")
+    pygame.init()
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Pong Game")
 
