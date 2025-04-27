ebef19681b4b8df3bc1cd071a59a2b4a9c14bbd7
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-27 09:15:24 +0000

    Apr 27, 2025, 9:15 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -29,9 +29,9 @@ while running:
         paddle1Y = paddle1Y - 1
     if keys[pygame.K_s] and paddle1Y < screenH - paddleH:
         paddle1Y = paddle1Y + 1
-    if keys[pygame.K_] and paddle1Y > 0:
+    if keys[pygame.K_UP] and paddle1Y > 0:
         paddle1Y = paddle1Y - 1
-    if keys[pygame.K_s] and paddle1Y < screenH - paddleH:
+    if keys[pygame.K_DOWN] and paddle1Y < screenH - paddleH:
         paddle1Y = paddle1Y + 1
 
     for event in pygame.event.get():
