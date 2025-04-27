a2e66c9a1cf2f6a48e12c0780cdcd497bc29ac0b
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-27 08:44:15 +0000

    Apr 27, 2025, 8:44 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -3,13 +3,14 @@ print("Hello from lesson 14_15_16")
 
 # Task 1
 
-running = True
 pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong Game")
 
+
+running = True
 while running:
     for event in pygame.event.game():
         if event.type == pygame.QUIT:
