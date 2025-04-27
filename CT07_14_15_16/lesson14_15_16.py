10ec9662e3a6d3cda9ed6e16910f8b035af83512
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-27 08:27:26 +0000

    Apr 27, 2025, 8:27 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -7,4 +7,4 @@ pygame.init()
 
 screen_width = 800
 screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_hiegth))
+screen = pygame.display.set_mode((screen_width, screen_height))
