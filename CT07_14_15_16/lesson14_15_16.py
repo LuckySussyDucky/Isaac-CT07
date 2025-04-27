bf907d98462b1981aa090e84d20a637e709a30f3
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-04-27 09:15:10 +0000

    Apr 27, 2025, 9:15 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -29,7 +29,7 @@ while running:
         paddle1Y = paddle1Y - 1
     if keys[pygame.K_s] and paddle1Y < screenH - paddleH:
         paddle1Y = paddle1Y + 1
-    if keys[pygame.K_w] and paddle1Y > 0:
+    if keys[pygame.K_] and paddle1Y > 0:
         paddle1Y = paddle1Y - 1
     if keys[pygame.K_s] and paddle1Y < screenH - paddleH:
         paddle1Y = paddle1Y + 1
