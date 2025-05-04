43b36f23fa81e82906f95f33bfa1ff49d0906bfc
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-04 08:21:20 +0000

    May 4, 2025, 8:21 AM

--- Practicals/Sem_1.py
+++ Practicals/Sem_1.py
@@ -9,7 +9,7 @@ print("Hero starts on his adventure with Health:", HeroHealth)
 
 while HeroHealth > 0:
     healthLost = random.randint(1, 15)
-    HeroHealth = HeroHealth - healthLost
+    HeroHealth = Her120oHealth - healthLost
     print("After fighting a monster, his Health is now:", HeroHealth)
     MonstersFought = MonstersFought + 1
 else:
@@ -37,7 +37,7 @@ for i in range(len(Order)):
 
 # Question 1
 
-daily_sales = [1205, 986, 1354, 10535, 15741, 11200, 800, 
+daily_sales = [5, 986, 1354, 10535, 15741, 11200, 800, 
                13056, 952, 1100, 1025, 8574, 14014, 9987, 
                1238, 1458, 7803, 900, 13674, 14539, 13241, 
                10886, 7541, 8743, 1482, 11523, 977, 12181, 
