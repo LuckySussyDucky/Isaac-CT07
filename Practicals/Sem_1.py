61c4c489fa2a2d68dd5a4de7420e687bb446e028
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-04 08:30:26 +0000

    May 4, 2025, 8:30 AM

--- Practicals/Sem_1.py
+++ Practicals/Sem_1.py
@@ -60,4 +60,4 @@ for sales in daily_sales:
     if sales < lowestSale:
         lowestSale = sales
 counter = (daily_sales.index(min(daily_sales)))
-print(counter, "August has the highest sales of $" + lowestSale)
+print(counter, "August has the highest sales of $" + str(lowestSale))
