931ed87367d23145829c65713b16c62ef66115a4
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-04 08:25:38 +0000

    May 4, 2025, 8:25 AM

--- Practicals/Sem_1.py
+++ Practicals/Sem_1.py
@@ -49,5 +49,5 @@ highestSale = 0
 for sales in daily_sales:
     if sales > highestSale:
         highestSale = sales
-counter = daily_sales.index(max(daily_sales))
+counter = (daily_sales.index(max(daily_sales))) + 1
 print(counter, "August has the highest sales of" , highestSale)
