fe1eb276c65215e599f12ff817c2de12240a35d9
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-11 09:00:11 +0000

    May 11, 2025, 9:00 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -53,10 +53,10 @@ while running:
     ballX = ballX + ballDX
     ballY = ballY + ballDY
 
-    if ballX <= 0 or ballX  >= screenW:
-        ballDX = ballDX * -1
-    if ballY <= 0 or ballY  >= screenH:
-        ballDY = ballDY * -1
+    # if ballX <= 0 or ballX  >= screenW:
+    #     ballDX = ballDX * -1
+    # if ballY <= 0 or ballY  >= screenH:
+    #     ballDY = ballDY * -1
     
     if ballX <= paddleBox1.right + ballR and ballY >= paddleBox1.top and ballY <= paddleBox1.bottom:
         ballDX = ballDX * -1
