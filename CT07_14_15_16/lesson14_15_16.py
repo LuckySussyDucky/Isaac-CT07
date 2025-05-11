11bf8f655506e20bb6eb46cc8a48cf05fece0775
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-11 08:41:30 +0000

    May 11, 2025, 8:41 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -35,7 +35,7 @@ running = True
 while running:
     # screen.fill(green)
     screen.blit(BackgroundImage, (0, 0))
-    screen.blit(TennisBallImage, )
+    screen.blit(TennisBallImage, (paddle1X, paddle1Y))
 
     pygame.draw.rect(screen, white, (paddle1X, paddle1Y, paddleW, paddleH))
     paddleBox1 = pygame.Rect(paddle1X, paddle1Y, paddleW, paddleH)
