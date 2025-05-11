80412e73dc729e12bfe55c8c3aa1544753ae341d
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-11 09:02:46 +0000

    May 11, 2025, 9:02 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -82,7 +82,7 @@ while running:
         player2score = player2score + 1
         ballDX = ballDX * -1
 
-    player1Score = scoreFont.render("Player 1;")
+    player1Score = scoreFont.render("Player 1: " + str())
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
