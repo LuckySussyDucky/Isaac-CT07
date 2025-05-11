02ddd07111f77983f26ebed324b1cf310adada00
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-11 08:10:24 +0000

    May 11, 2025, 8:10 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -39,7 +39,7 @@ while running:
     ballY = ballY + ballDY
 
     if ballY <= 0 or ballY  >= screenH:
-        ballY = ballY - 1
+        ball = ballY - 1
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w] and paddle1Y > 0:
