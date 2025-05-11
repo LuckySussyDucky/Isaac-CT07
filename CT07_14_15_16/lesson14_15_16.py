af595cc1ad45bb6e71f31f74cdf95b1d673c3093
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-11 08:48:00 +0000

    May 11, 2025, 8:48 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -37,7 +37,7 @@ while running:
     # screen.fill(green)
     screen.blit(BackgroundImage, (0, 0))
     screen.blit(TennisBallImage, (ballX - ballR, ballY - ballR))
-    screen.blit(TennisRacketImage, (paddle1X, paddle1Y))
+    screen.blit(rotate(TennisRacketImage), (paddle1X, paddle1Y))
     screen.blit(TennisRacketImage, (paddle2X, paddle2Y))
 
     # pygame.draw.rect(screen, white, (paddle1X, paddle1Y, paddleW, paddleH))
