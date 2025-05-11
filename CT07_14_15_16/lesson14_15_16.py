26c54240ba3646a1e1bbd49f8d0adf0b937fc3da
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-11 08:40:29 +0000

    May 11, 2025, 8:40 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -29,7 +29,7 @@ ballDX = 0.5
 ballDY = 0.5
 
 BackgroundImage = pygame.image.load("CT07_14_15_16/Grass Court.jpg")
-TennisBallImage = pygame.image.load("CT07_14_15_16/Grass Court.jpg")
+TennisBallImage = pygame.image.load("CT07_14_15_16/Tennis Ball.png")
 
 running = True
 while running:
