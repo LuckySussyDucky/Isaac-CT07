eeb371199308b3b146f8223fdf8be0ea69cc32db
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-18 08:53:54 +0000

    May 18, 2025, 8:53 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -113,7 +113,7 @@ while running:
         running = False
     if player2Score >= 3:
         screen.blit(player2_win_text, ((screenW // 2) - (player2_win_text.get_width() // 2),(screenH // 2) - (player2_win_text.get_height())))
-        paue = True
+        pause = True
         running = False
 
     if pause == True:
