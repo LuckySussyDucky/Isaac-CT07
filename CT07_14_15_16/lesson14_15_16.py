953adeb8e631aa241e68828cc4f318fd1ec023b2
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-18 08:26:02 +0000

    May 18, 2025, 8:26 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -100,11 +100,13 @@ while running:
 
     if player1Score >= 3:
         screen.blit(player1_win_text, ((screenW // 2) - (player1_win_text.get_width() // 2),(screenH // 2) - (player1_win_text.get_height())))
+        
         running = False
     if player2Score >= 3:
         screen.blit(player2_win_text, ((screenW // 2) - (player2_win_text.get_width() // 2),(screenH // 2) - (player2_win_text.get_height())))
-        running = False
         
+        running = False
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runnning = False
