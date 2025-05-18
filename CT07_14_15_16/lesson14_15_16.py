921398ec0cf26725bbece295a22f1266f3296314
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-05-18 08:26:32 +0000

    May 18, 2025, 8:26 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -1,4 +1,4 @@
-import timr
+import time
 import pygame
 from pygame.transform import rotate
 print("Hello from lesson 14_15_16")
@@ -101,7 +101,7 @@ while running:
 
     if player1Score >= 3:
         screen.blit(player1_win_text, ((screenW // 2) - (player1_win_text.get_width() // 2),(screenH // 2) - (player1_win_text.get_height())))
-        
+        time
         running = False
     if player2Score >= 3:
         screen.blit(player2_win_text, ((screenW // 2) - (player2_win_text.get_width() // 2),(screenH // 2) - (player2_win_text.get_height())))
